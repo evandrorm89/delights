a450e98ecbae7353400d9b2cf5f9b7d03feb3d23
Evandro <evandrorm89@gmail.com>
2023-01-19 14:22:16 -0300

    adding views and models

--- django_delights/models.py
+++ django_delights/models.py
@@ -14,6 +14,9 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=200)
     price = models.FloatField()
 
+    def __str__(self):
+        return self.title
+
 class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
@@ -21,3 +24,10 @@ class RecipeRequirement(models.Model):
 
     def __str__(self):
         return f'{self.quantity} {self.ingredient.unit} of {self.ingredient.name} to create {self.menu_item.title}'
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.menu_item.title} purchased at {self.timestamp}'
--- django_delights/views.py
+++ django_delights/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import MenuItem
 # Create your views here.
 def index(request):
     return HttpResponse('Ok Ok Ratinho!')
+
+class IndexView(generic.ListView):
+    model = MenuItem
