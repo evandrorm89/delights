d8e50f8d320d1465109a89b1f77c5774c2f615fc
Evandro <evandrorm89@gmail.com>
2023-01-20 15:59:30 -0300

    Adding some views

--- django_delights/urls.py
+++ django_delights/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('menu/', views.MenuView.as_view(), name='menu'),
+    path('ingredients/list', views.IngredientsView.as_view(), name='ingredients'),
+    path('purchases/list', views.PurchasesView.as_view(), name='purchases'),
 ]
--- django_delights/views.py
+++ django_delights/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
-from .models import MenuItem
+from .models import MenuItem, Ingredient, Purchase
 # Create your views here.
-def index(request):
-    return HttpResponse('Ok Ok Ratinho!')
+def home(request):
+    context = {'name': 'Teste'}
+    return render(request, 'django_delights/home.html', context)
 
-class IndexView(generic.ListView):
+class MenuView(generic.ListView):
     model = MenuItem
+
+class IngredientsView(generic.ListView):
+    model = Ingredient
+
+class PurchasesView(generic.ListView):
+    model = Purchase
